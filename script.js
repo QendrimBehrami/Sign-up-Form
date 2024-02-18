38fa80c3f3028a6b6a03d9fc988075ea70f29e2c
Qendrim Behrami <behrami@in.tum.de>
2024-02-18 14:31:04 +0100

    Improve validation styles

--- script.js
+++ script.js
@@ -4,16 +4,28 @@
  * @param {HTMLInputElement} passwordConfirmElement
  */
 function comparePasswords(passwordElement, passwordConfirmElement) {
+  //   if (!passwordElement.value || !passwordConfirmElement.value) {
+  //     passwordElement.style["border"] = "1.5px solid black";
+  //     passwordConfirmElement.style["border"] = "1.5px solid black";
+  //   } else {
+  //     if (passwordConfirmElement.value === passwordElement.value) {
+  //       passwordElement.style["border"] = "2px solid green";
+  //       passwordConfirmElement.style["border"] = "2px solid green";
+  //     } else {
+  //       passwordElement.style["border"] = "2px solid red";
+  //       passwordConfirmElement.style["border"] = "2px solid red";
+  //     }
+  //   }
   if (!passwordElement.value || !passwordConfirmElement.value) {
-    passwordElement.style["border"] = "1.5px solid black";
-    passwordConfirmElement.style["border"] = "1.5px solid black";
+    passwordElement.setCustomValidity("");
+    passwordConfirmElement.setCustomValidity("");
   } else {
     if (passwordConfirmElement.value === passwordElement.value) {
-      passwordElement.style["border"] = "2px solid green";
-      passwordConfirmElement.style["border"] = "2px solid green";
+      passwordElement.setCustomValidity("");
+      passwordConfirmElement.setCustomValidity("");
     } else {
-      passwordElement.style["border"] = "2px solid red";
-      passwordConfirmElement.style["border"] = "2px solid red";
+      passwordElement.setCustomValidity("Invalid field.");
+      passwordConfirmElement.setCustomValidity("Invalid field.");
     }
   }
 }
